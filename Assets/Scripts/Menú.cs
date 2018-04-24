935528f7999509f30a9b3821385b66d5a91e743e
Pablo Martín Navarro <pablma13@ucm.es>
2018-04-24 20:21:26 +0100

    Menú de juego

---
+++ Assets/Scripts/Menú.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Menú : MonoBehaviour {
+
+	public AudioMixer audio;
+
+	public void Jugar()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	public void Volumen (float volumen)
+	{
+		audio.SetFloat ("Volumen", volumen);
+	}
+}
