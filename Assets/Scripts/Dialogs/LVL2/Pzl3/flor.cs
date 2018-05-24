7fd1b13b8c4c1abcb9e74e82aeb8dd301975c980
pablma13 <pablma13@ucm.es>
2018-05-25 00:59:36 +0100

    SirTabaiba&Machango.1.0

---
+++ Assets/Scripts/Dialogs/LVL2/Pzl3/flor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flor : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject CajaTxt, txt, icono, caja;
+	private bool ready;
+
+	void Awake ()
+	{
+		txt.SetActive (false);
+		icono.SetActive (false);
+		ready = false;
+	}
+
+	private void OnTriggerEnter2D (Collider2D coll) 
+	{
+		if (coll.gameObject.CompareTag ("Player")) 
+		{
+			ShowTxt ();
+			Invoke("ShowIcon", 1.5f);
+			ready = true;
+		}
+	}
+
+	private void ShowIcon ()
+	{
+		icono.SetActive (true);
+	}
+
+	private void ShowTxt () 
+	{
+		txt.SetActive (true);
+		CajaTxt.SetActive (true);
+	}
+
+	void Update ()
+	{
+		if (ready && caja == null)
+			Terminate ();
+	}
+
+	private void Terminate()
+	{
+		txt.SetActive (false);
+		icono.SetActive (false);
+		CajaTxt.SetActive (false);
+		Destroy (this.gameObject);
+	}
+}
